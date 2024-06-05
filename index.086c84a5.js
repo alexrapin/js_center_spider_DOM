40d54c45aba38f77aca44586f3c1b6f9e6c84911
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-05 14:40:51 +0300

    make build

---
+++ index.086c84a5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left="".concat((t.clientWidth-e.clientWidth)/2,"px"),e.style.top="".concat((t.clientHeight-e.clientHeight)/2,"px");
+//# sourceMappingURL=index.086c84a5.js.map
